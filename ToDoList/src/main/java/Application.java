85d2aa62d137e4c6ac2b84ec0d8eebb415e553ed
DanielSaanu <danielsaanu01@gmail.com>
2024-02-09 19:38:37 +0000

    Add files via upload

---
+++ ToDoList/src/main/java/Application.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Application
+{
+    public static void main (String[]Args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String task = sc.nextLine();
+    }
+    public static String TaskList
+}
